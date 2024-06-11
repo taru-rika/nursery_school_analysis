8fc89f7db15c2b23407805b2c685afd1bc423103
taru-rika <nanaysd.rk17@gmail.com>
2024-06-11 15:03:21 +0900

    次元削減手法修正&wordcloud実装

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from streamlit_folium import st_folium
 import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
+import numpy as np
 import urllib
 
 # ページ設定
@@ -15,53 +16,60 @@ st.set_page_config(
 
 pages = ["search", "recommend"]
 page = st.sidebar.radio("""### menu""",pages)
+facility_list =[]
 
 if page == "search":
-	st.title("幼稚園・保育園選びお助けアプリ")
+	st.title("専門家向け保育園・幼稚園評価分析システム")
 	st.markdown("""#### 概要：
-		あなたと幼稚園（保育園）両者のニーズにマッチした幼稚園（保育園）をレコメンドします．""")
+		教育の専門家や自治体が保育園・幼稚園を分析する際のサポートシステムとなることを目標とします""")
 	
-	st.markdown("---")
-	st.markdown("""#### 検索条件設定""")
-	col1, col2, col3 = st.columns(3)
-
-	with col1:
-		st.markdown("""#### 1 地点""")
-		plc=st.text_input("以下に入力してください.ex)茗荷谷駅",value="")
-	
-	#半径を指定する場合
-	with col2:
-		st.markdown("""#### 2 範囲""")
-		plc_range = st.slider('地点を中心とした距離(m)',value=500, min_value=0, max_value=3000) #スライダーをつける
-		st.write("地点からの直線距離{:,}m".format(plc_range))
-		
-		#施設の種類
-		facility_type=[
-			"認可保育所",
-			"認証保育所A型・B型",
-			"認定こども園",
-			"認可外保育施設",
-			"乳児院",
-			"児童養護施設",
-			"児童自立生活援助事業[自立援助ホーム]",
-			"母子生活支援施設",
-			"児童自立支援施設",
-		]
-
-	with col3:
-		st.markdown("""#### 3 施設の種類""")
-		ty=st.selectbox("以下から一つ選んでください", facility_type)
-		st.write('選択したオプション：',ty)
-
-	button = st.button('検索')
-	st.markdown("---")
+#	st.markdown("---")
+#	st.markdown("""#### 検索条件設定""")
+#	col1, col2, col3 = st.columns(3)
+#
+#	with col1:
+#		st.markdown("""#### 1 地点""")
+#		plc=st.text_input("以下に入力してください.ex)茗荷谷駅",value="")
+#	
+#	#半径を指定する場合
+#	with col2:
+#		st.markdown("""#### 2 範囲""")
+#		plc_range = st.slider('地点を中心とした距離(m)',value=500, min_value=0, max_value=3000) #スライダーをつける
+#		st.write("地点からの直線距離{:,}m".format(plc_range))
+#		
+#		#施設の種類
+#		#facility_type=[
+#		#	"認可保育所",
+#		#	"認証保育所A型・B型",
+#		#	"認定こども園",
+#		#	"認可外保育施設",
+#		#	"乳児院",
+#		#	"児童養護施設",
+#		#	"児童自立生活援助事業[自立援助ホーム]",
+#		#	"母子生活支援施設",
+#		#	"児童自立支援施設",
+#		#]
+#		facility = pd.read_csv('./nursery_school/nursery_data.csv')
+#		facility_type = np.array(facility['事業所種'])
+#		facility_type = set(facility_type)
+#		print(facility_type)
+#		facility_list.append(facility['事業所名称'])
+#
+#	with col3:
+#		st.markdown("""#### 3 施設の種類""")
+#		ty=st.selectbox("以下から一つ選んでください", facility_type)
+#		st.write('選択したオプション：',ty)
+#
+#	button = st.button('検索')
+#	st.markdown("---")
 
 	m = folium.Map(
 	    # 地図の中心位置の指定(今回は東京駅を指定)
-	    location=[35.681236 , 139.767125], 
+	    location=[35.72 , 139.75], 
 	    # タイル、アトリビュートの指定
-	    tiles='https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',
-	    attr= ty + '(令和5年度10月)',
+	    tiles='https://cyberjapandata.gsi.go.jp/xyz/airphoto/{z}/{x}/{y}.png',
+	    attr= '(令和5年度10月)',
+	    #attr= ty + '(令和5年度10月)',
 	    # ズームを指定
 	    zoom_start=15
 	)
@@ -69,30 +77,27 @@ if page == "search":
 	#マップに検索結果を表示する
 	# 地図の中心の緯度/経度、タイル、初期のズームサイズを指定します。
 	
+	print("facility_list : ", facility_list)
 	#表示するデータを読み込み
-	df = pd.read_csv('./nursery_school/202310-2-1_' + ty +'.csv', encoding="shift-jis", nrows=10)
+	df = pd.read_csv('./nursery_school/nursery_data.csv')
+	#df = pd.read_csv('./nursery_school/202310-2-1_' + ty +'.csv', encoding="shift-jis", nrows=10)
 	
 	# 読み込んだデータ(緯度・経度、ポップアップ用文字、アイコンを表示)
 	marker_cluster = MarkerCluster()
 	for i, row in df.iterrows():
-	    # ポップアップの作成(都道府県名＋都道府県庁所在地＋人口＋面積)
-	    pop=f"{row['施設名']}({row['設置']})<br>　定員…{row['認可定員']:,}人"
-	
-	    address = row['所在地']
-	    makeUrl = "https://msearch.gsi.go.jp/address-search/AddressSearch?q="
-	    s_quote = urllib.parse.quote(address)
-	    response = requests.get(makeUrl + s_quote)
-	    lonlat=response.json()[0]["geometry"]["coordinates"]
-	    print(lonlat)
+	    # ポップアップの作成
+	    #pop = f'<img src= "data:image/png;base64,{row['img_base64']}" >'
+	    pop = f'{row["事業所名称"]} <img src="data:image/png;base64,{row["img_base64"]}">'
 	    folium.Marker(
 	        # 緯度と経度を指定
-	        location=[lonlat[1],lonlat[0]],
+                location=[row['lat'],row['lon']],
 	        # ツールチップの指定(都道府県名)
-	        tooltip=row['施設名'],
+	        tooltip=row['事業所名称'],
 	        # ポップアップの指定
-	        popup=folium.Popup(pop, max_width=300),
+	        popup=folium.Popup(pop, max_width=400),
 	        # アイコンの指定(アイコン、色)
-	        icon=folium.Icon(icon="home",icon_color="white", color="red")
+	        #icon=folium.Icon(color="red")
+	        icon=folium.Icon(icon='home', icon_color=row['feature_color'], color='white')
 	    ).add_to(marker_cluster)
 	marker_cluster.add_to(m)
 	st_data = st_folium(m, width=1200, height=800)
--- bert_nurserydata.py
+++ bert_nurserydata.py
@@ -1,12 +1,18 @@
-from transformers import BertTokenizer, BertModel
+from transformers import BertJapaneseTokenizer, BertModel
+#from transformers import BertTokenizer, BertModel
 import torch
 from sklearn.decomposition import PCA
 import numpy as np
 import pandas as pd
+import base64
+from io import BytesIO
+from wordcloud import WordCloud
+import colorsys
 
 # BERTのトークナイザーとモデルを読み込む
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased')
+tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+model_bert = BertModel.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+model_bert.eval()
 
 # 文の例として、テキストを定義
 df = pd.read_csv('./nursery_school/nursery_data.csv')
@@ -14,39 +20,73 @@ df = pd.read_csv('./nursery_school/nursery_data.csv')
 text_list = np.array(df['事業所の理念'])
 print(text_list)
 #text = "The quick brown fox jumps over the lazy dog."
-feature_list = []
 feature_color = []
+feature_list = []
+img_list = []
+
+def calc_embedding(text):
+  bert_tokens = tokenizer.tokenize(text)
+  ids = tokenizer.convert_tokens_to_ids(["[CLS]"] + bert_tokens[:126] + ["[SEP]"])
+  tokens_tensor = torch.tensor(ids).reshape(1, -1)
+
+  with torch.no_grad():
+    output = model_bert(tokens_tensor)
+
+  #return output[1].numpy()
+  return output.last_hidden_state.squeeze(0).numpy()
+
+def float_to_rgb(float_values):
+    # 各float値が[0, 1]の範囲であることを確認
+    assert all(0.0 <= val <= 1.0 for val in float_values), "Float values should be in the range [0, 1]"
+    # 平均値を計算
+    mean_value = sum(float_values) / len(float_values)
+    # 平均値に基づいてスケーリングを調整
+    adjusted_values = [(val - mean_value) * 0.5 + 0.5 for val in float_values]
+    # HSLの色相、彩度、輝度を計算（ここでは彩度と輝度を固定値にしています）
+    hue = adjusted_values[0]  # 色相
+    saturation = adjusted_values[1]  # 彩度
+    lightness = adjusted_values[2]  # 輝度
+    # HSLからRGBへの変換
+    r, g, b = colorsys.hls_to_rgb(hue, lightness, saturation)
+    # [0, 255]の範囲にスケーリングし、整数に変換
+    rgb_values = [int(r * 255), int(g * 255), int(b * 255)]
+    return rgb_values
 
 for text in text_list:
-	# 文をトークン化し、BERTの入力形式に変換
-	input_ids = tokenizer(text, return_tensors='pt', padding=True, truncation=True)['input_ids']
-	
-	# BERTモデルに入力し、特徴量を取得
-	with torch.no_grad():
-	    outputs = model(input_ids)
-	    features = outputs.last_hidden_state.squeeze(0).numpy()  # BERTの出力から特徴量行列を取得し、NumPy配列に変換
-	
+	# 特徴量行列を取得
+	feature = calc_embedding(text)
 	# PCAで次元削減を行う
 	pca = PCA(n_components=3)  # 3次元に次元削減
-	features_3d = pca.fit_transform(features)
-	
-	# 変換された特徴量行列の形状を確認
-	print("Shape of 3D features:", features_3d.shape)
-	feature_list.append(features_3d);
-	
-	# 各事業所の平均特徴量を計算
-	mean_features = np.mean(features_3d, axis=0)
-	# 平均特徴量を0から255の範囲にスケーリングして、カラーコードを求める
-	min_val = np.min(mean_features)
-	max_val = np.max(mean_features)
-	scaled_mean_features = (mean_features - min_val) / (max_val - min_val) * 255
-	color = '#%02x%02x%02x' % (int(scaled_mean_features[0]), int(scaled_mean_features[1]), int(scaled_mean_features[2]))
-	
-	# カラーコードを出力
-	print("Color for the office:", color)	
+	features_3d = pca.fit_transform(feature)
+	result_r = pca.explained_variance_ratio_[0]
+	result_g = pca.explained_variance_ratio_[1]
+	result_b = pca.explained_variance_ratio_[2]
+	rgb_list = [result_r, result_g, result_b]
+	print(rgb_list)
+	rgb_values = float_to_rgb(rgb_list)
+	color = '#%02x%02x%02x' % (int(rgb_values[0]), int(rgb_values[1]), int(rgb_values[2]))
+	#print(color)
 	feature_color.append(color)
 
-df['事業所の理念_feature'] = feature_list
+	# wordcloudのエンコード情報を取得
+	#フォントの設定
+	#font_path="/usr/share/fonts/truetype/fonts-japanese-gothic.ttf"
+	#font_path='/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc'
+	font_path='/Users/rikatarumi/Library/Fonts/NotoSansJP-VariableFont_wght.ttf'
+	#色の設定
+	colormap="Paired"
+	# WordCloudの生成（日本語フォントを指定）
+	wordcloud = WordCloud(font_path=font_path, width=400, height=200, background_color='white', stopwords=["1", "2", "3", "4"]).generate(text)
+	# WordCloud画像をバイナリ形式で保存
+	img = BytesIO()
+	wordcloud.to_image().save(img, format='PNG')
+	img.seek(0)
+	# 画像をBase64エンコード
+	img_base64 = base64.b64encode(img.getvalue()).decode('utf-8')
+	#print(img_base64)
+	img_list.append(img_base64)
+
 df['feature_color'] = feature_color
+df['img_base64'] = img_list
 print(df)
 df.to_csv("./nursery_school/nursery_data.csv", index=False)
